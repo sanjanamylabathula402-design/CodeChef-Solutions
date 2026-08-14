8608126b589147665a7cf7ae64930fc6dbcb11a8
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-14 15:10:19 +0530

    ✅ HDHCEH15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

--- codechef/medium/HDHCEH15/solution.java
+++ codechef/medium/HDHCEH15/solution.java
@@ -1,27 +1,19 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Account{
-    int t_id;
-    float amount;
+class Dog{
+    String breed;
+    String name;
 }
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Account transaction1 = new Account();
- 
-       // access data members t_id and amount from object transcaction1 and then assign values to them
-       
-       transaction1.t_id=1003;
-       transaction1.amount=50000.0f;
-       
-
-        // access  t_id from transcation1 to print 
-        System.out.println("The transaction ID is "+ transaction1.t_id);
-        // access  amount from transcation1 to print 
-        System.out.println("The amount spent is "+ transaction1.amount);
-	}
+class Codechef {
+    public static void main(String[] args) {
+        // Create an object for Dog class here 
+        Dog doggy=new Dog();
+        
+        // assign values for breed and name, breed is "pug" and name is "bob"
+        doggy.breed="pug";
+        doggy.name="bob";
+        
+        // print the result
+        System.out.println("Breed: " + doggy.breed);
+        System.out.println("Name: " + doggy.name);
+    }
 }
