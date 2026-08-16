22efe9cb39d0bf106b0b5cb9630a953f776239da
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-16 23:11:42 +0530

    ✅ QUALIFY - Rating 593 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/easy/QUALIFY---Rating-593/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+while(t-->0)
+{
+   
+    int n=sc.nextInt();
+    if(n % 4==0)
+    {
+        System.out.println("good");
+    }
+    else 
+    {
+        System.out.println("not good");
+    }
+}
+	}
+}
