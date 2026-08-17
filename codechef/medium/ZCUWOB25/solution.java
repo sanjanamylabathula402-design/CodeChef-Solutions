4ca06fb354232e3489bee9e62ab51ba599794440
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-17 16:10:40 +0530

    ✅ ZCUWOB25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/ZCUWOB25/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        for (int i = 0; i < rows; i++) {  
+            for (int j = 0; j < cols; j++) {  
+                int num = i + j; 
+                if (num % 2 == 0) {
+                    System.out.print("EVEN  ");
+                } else {
+                    System.out.print(num + "   ");
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
