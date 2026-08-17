3a4e850ca5d3eadce90cb1db00f379bdf35e7127
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-17 15:51:37 +0530

    ✅ CTMNMF06 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/CTMNMF06/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+
+        String correctPassword = "JavaRocks";
+        String enteredPassword = "JavaRocks";
+
+        // Write the condition below in the while loop
+        while (!enteredPassword.equals(correctPassword) ) {
+            System.out.println("Invalid Password");
+
+        }
+
+        System.out.println("Access Granted!");
+
+    }
+}
