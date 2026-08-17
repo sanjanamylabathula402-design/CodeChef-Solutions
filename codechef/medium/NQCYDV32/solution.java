11425447d476fc5eb600636b715e1e71fa3fe30f
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-17 22:25:33 +0530

    ✅ NQCYDV32 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/NQCYDV32/solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class CodeChef {
+    public static void main(String[] args) {
+        int numerator = 10;
+        int denominator = 1; // Change the denominator to 1 to avoid the error and get correct output.
+        int result = numerator / denominator;
+        System.out.println("Age is " + result);
+}
+}
