87b1f3cc6686090f9f2b41b7fe0553f3e50a8b22
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-17 15:49:30 +0530

    ✅ CTMNMF04 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/CTMNMF04/solution.java
+++ codechef/medium/CTMNMF04/solution.java
@@ -1,15 +1,16 @@
 class Codechef {
     public static void main(String[] args) {
-        
-        // Declare a boolean condition
-        boolean isRunning = true;
 
-        // While loop to print a message while the condition is true
-        while (isRunning) {
-            System.out.println("The process is running...");
-            
-            // Changing the condition to false (breaking the loop)
-            isRunning = false; 
+        String correctPassword = "JavaRocks";
+        String enteredPassword = "JavaRocks";
+
+        // Write the condition below in the while loop
+        while (!enteredPassword.equals(correctPassword) ) {
+            System.out.println("Invalid Password");
+
         }
+
+        System.out.println("Access Granted!");
+
     }
 }
