9df566a3a11b1bebd33a9c8a7545d2c63576014e
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-17 15:59:32 +0530

    ✅ POOWVO15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/POOWVO15/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        int correctPIN = 1234;
+        boolean isVerified = false; 
+        while (!isVerified) {
+            int userPIN = scanner.nextInt();
+            if (userPIN == correctPIN) {
+                isVerified = true; 
+                System.out.println("Access granted!"); 
+            } else {
+                System.out.println("Invalid PIN. Try again."); 
+            }
+        }
+
+        scanner.close();
+    }
+}
