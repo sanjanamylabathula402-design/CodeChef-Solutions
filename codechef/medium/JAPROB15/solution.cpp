8796ceebda590034c552e08e7af3e91f1ebfbf70
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-19 12:30:39 +0530

    ✅ JAPROB15 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/JAPROB15/solution.cpp
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Codechef {
+
+    // Task - Complete the below functions
+    //--------------------------------------------------------------
+    public static int checkAnswer(String userAnswer, String correctAnswer) {
+
+        if (________) { // Check if the userAnswer is equal to correct answer
+            System.out.println("Answer is correct!");
+            
+
+            // return 1
+
+        } else {
+
+            System.out.println("Your answer is wrong! The correct answer is " + correctAnswer + ".");
+            
+            
+            //  return 0;
+        }
+
+    }
+
+
+    public static int updateScore(int score, int result) {
