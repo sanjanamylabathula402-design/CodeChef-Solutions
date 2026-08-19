f048d47414368806e2ae924474f834cbc81e9915
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-19 10:57:31 +0530

    ✅ MONKCI09 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MONKCI09/solution.java
+++ codechef/medium/MONKCI09/solution.java
@@ -1,24 +1,20 @@
 import java.util.ArrayList;
 
-public class Main {
+class Codechef {
     public static void main(String[] args) {
-        ArrayList<String> todoList = new ArrayList<>();
+        ArrayList<String> shoppingList = new ArrayList<>();
         
-        // Adding initial tasks
-        todoList.add("Buy groceries");
-        todoList.add("Do laundry");
-        todoList.add("Pay bills");
-        todoList.add("Call mom");
-        todoList.add("Walk dog");
+        shoppingList.add("Milk");
+        shoppingList.add("Eggs");
+        shoppingList.add("Bread");
+        shoppingList.add("Cheese");
+        shoppingList.add("Apples");
+        shoppingList.add("Chicken");
+        shoppingList.add("Rice");
         
-        System.out.println("Original list: " + todoList);
+        shoppingList.remove(3);
+        shoppingList.remove("Bread");
         
-        // Remove task at index 2 ("Pay bills")
-        todoList.remove(2);
-        System.out.println("After removing index 2: " + todoList);
-        
-        // Remove "Call mom" by object
-        todoList.remove("Call mom");
-        System.out.println("Final to-do list: " + todoList);
+        System.out.println("Final shopping list: " + shoppingList);
     }
 }
