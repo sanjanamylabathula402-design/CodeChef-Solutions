24ff52c0b28fbe18f3993bebf39165621cea39f3
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-08-31 14:16:40 +0530

    ✅ POLMDB05B | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/POLMDB05B/solution.cpp
@@ -0,0 +1,18 @@
+  material: "Organic Cotton"
+});
+
+// DOCUMENT 2: A Camera (Focus on Tech fields)
+db.products.insertOne({
+  size: "Extra Large",
+  // Added fields
+  price: 59.99,
+  category: "Clothing",
+db.products.drop();
+
+// --- YOUR TASK START ---
+
+// DOCUMENT 1: A Shirt (Focus on Clothing fields)
+db.products.insertOne({
+  item_name: "Vintage Denim Jacket",
+
+const db = connect('mongodb://localhost:27017/marketplace_db');
