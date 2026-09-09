c5de2e8af0fcaa5dbfe41f0b82dcee8ddaecfdcb
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-09 21:49:25 +0530

    ✅ ADDIS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/ADDIS/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    HashMap<Integer,Integer> hm=new HashMap<>();
+		    int mFreq=0;
+		    for(int i=0;i>n;i++)
+		    {
+		    int x=sc.nextInt();
+		    hm.put(x,hm.getOrDefault(x,0)+1);
+		    mFreq=Math.max(mFreq,hm.get(x));
+		}
+		System.out.println((mFreq+1)/2);
+		}
+	}
+	}
