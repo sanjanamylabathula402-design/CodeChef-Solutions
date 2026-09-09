e2fd3b7615c3fdb54debc2d5753fb4a44b733a59
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-09 20:27:21 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/UNQSHF/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+while (t-->0)
+    String t=sc.nextChar();
+    int n=sc.nextInt();
+    String a=sc.nextChar();
+    String b=sc.nextChar();
+    {
+        
+   }
+
+	}
+}
