79fb720f2c27223a8680738841a034e5fcfe1756
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-16 20:30:41 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/BUSSEAT/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+Scanner sc=new Scanner(System.in);
+int t=sc.mextInt();
+while(t-->0)
+int n=sc.nextInt();
+int k=sc.nextInt();
+if (k > n)
+{
+                System.out.println(k - n);
+            } else {
+                System.out.println(0);
+	}
+}
+}
