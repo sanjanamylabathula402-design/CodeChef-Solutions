ad813538b1cc4683463a24de66f9d82a29b71766
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-20 21:55:10 +0530

    ✅ TWOSUMM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/TWOSUMM/solution.java
@@ -0,0 +1,12 @@
+public static int[] findPair(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int required = target - nums[i];
+        if (map.containsKey(required)) {
+            return new int[]{map.get(required), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[]{-1, -1};
+
+}
