7997afde17d826b6d46bdd5d9de81c52e204bc9a
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-21 14:50:54 +0530

    ✅ GSJ215 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/GSJ215/solution.java
@@ -0,0 +1,17 @@
+// Debug the following code to solve the problem
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        
+        for(int i=0; i<t; i++)
+        {
+            int n = read.nextInt();
+            System.out.println(n*2);
+        }
+	}
+}
