5c5d1a7f5afc1f987a7226f89a5682f6f7d842ea
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-21 14:47:27 +0530

    ✅ GSJ213B | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/GSJ213B/solution.java
@@ -0,0 +1,19 @@
+// Change the 'custom inputs' below and click 'run'
+// Click on 'Submit' once you have tried out to proceed to the next problem
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int n = read.nextInt();
+            System.out.println(n+1);
+        }
+	}
+}
