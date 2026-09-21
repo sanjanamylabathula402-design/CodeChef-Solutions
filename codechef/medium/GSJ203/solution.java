e94a24451e7507dbedd201ece1f3fdda777595b7
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-21 14:41:14 +0530

    ✅ GSJ203 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/GSJ203/solution.java
+++ codechef/medium/GSJ203/solution.java
@@ -1,18 +1,12 @@
-// Click 'Submit' to view the result
+// Update the blanks in the code below
+import java.util.Scanner;
+
 class Codechef
 {
 	public static void main (String[] args)
 	{
-		int a = -5; // Integer variable
-		System.out.println(a);
-		
-		double b = 4.0; // Double variable
-		System.out.println(b);
-		
-		String s = "1234abc"; //String variable
-		System.out.println(s);
-		
-		String[] array = {"1", "ab", "3", "name"}; // Array with string values
-		System.out.print(array[0]);
+        Scanner objName = new Scanner(System.in);
+        int N = objName.nextInt();
+		System.out.println(N);
 	}
 }
