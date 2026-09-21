c20e5164fe3dfe8e88a3af36f0615c6bd324341c
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-21 14:42:20 +0530

    ✅ GSJ208 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/GSJ208/solution.java
@@ -0,0 +1,17 @@
+//  Update the '_' in the code below
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        String A = read.next();
+        String B = read.next();
+        String C = read.next();
+        String D = read.next();
+        String E = read.next();
+        System.out.print(A + " " + B + " " + C + " " + D + " " + E );
+	}
+}
