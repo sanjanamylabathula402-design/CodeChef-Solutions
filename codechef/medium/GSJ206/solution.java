5dbdaaef8560add40dde9c828c85109361b6f4f8
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-21 14:41:41 +0530

    ✅ GSJ206 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/GSJ206/solution.java
@@ -0,0 +1,14 @@
+// replace the '_' with the correct syntax
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner objName = new Scanner(System.in);
+        int A = objName.nextInt();
+        int B = objName.nextInt();
+        int C = objName.nextInt();
+		System.out.println(A + " " + B + " " + C);
+	}
+}
