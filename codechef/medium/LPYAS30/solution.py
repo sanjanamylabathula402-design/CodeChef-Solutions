28e9fa82de61ff85d584f8a87ea727907b0749c6
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-23 11:15:13 +0530

    ✅ LPYAS30 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LPYAS30/solution.py
@@ -0,0 +1,8 @@
+# cook your dish here
+
+
+# Declare a variable "temperature" and initialize it with a value of 25.5
+temperature=25.5
+
+print("Celsius -",temperature)
+print("Kelvin -",temperature + 273)
