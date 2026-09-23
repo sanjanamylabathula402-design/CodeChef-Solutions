3d1f95e8b172dbe7de0428f3ac439775feeaaf72
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-23 12:05:26 +0530

    ✅ LPYAS40 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LPYAS40/solution.py
+++ codechef/medium/LPYAS40/solution.py
@@ -1,4 +1,9 @@
-one = "Coding"
-two = "Chaf"
-two=two[:2]+'e'+two[3:]
-print(one + " " + two)
+# cook your dish here
+string1 = "coding"
+string2 = "on"
+string3 = "codechef"
+string = string1 + " "+ string2 + " " + string3
+print(string1 + " - "  + str(len(string1)))
+print(string2 + " - " + str(len(string2)))
+print(string3 +" - "+ str(len(string3)))
+print(string +" - "+ str(len(string)))
