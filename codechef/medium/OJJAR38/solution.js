6a2112615107103d9a4a540a99fe49ca5fd4c542
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-23 11:06:17 +0530

    ✅ OJJAR38 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/OJJAR38/solution.js
@@ -0,0 +1,11 @@
+let scores = [10, 45, 23, 8];
+let totalScore = 0; 
+
+// complete the code 
+
+// Use forEach to go through each score
+
+// Add the current score to the totalScore
+  scores.forEach(score=>{totalScore+=score;});
+
+console.log("Total Score:", totalScore); 
