4246f031a9d445aeb15cfb4b13ff3158b64c04c4
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-23 12:05:27 +0530

    ✅ LPYAS40B | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LPYAS40B/solution.py
@@ -0,0 +1,9 @@
+# cook your dish here
+string1 = "coding"
+string2 = "on"
+string3 = "codechef"
+string = string1 + " "+ string2 + " " + string3
+print(string1 + " - "  + str(len(string1)))
+print(string2 + " - " + str(len(string2)))
+print(string3 +" - "+ str(len(string3)))
+print(string +" - "+ str(len(string)))
