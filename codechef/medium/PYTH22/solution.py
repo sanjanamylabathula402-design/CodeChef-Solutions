bf38094c016ee2f13759cfd992845ea10f10033e
sanjanamylabathula402-design <sanjanamylabathula402@gmail.com>
2026-09-26 23:18:04 +0530

    ✅ PYTH22 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-26

--- codechef/medium/PYTH22/solution.py
+++ codechef/medium/PYTH22/solution.py
@@ -1,5 +1,4 @@
-# Fix one of the variable names to make the code work
-
-first_name = "Code"
-last_name = "Chef"
-print(first_name, last_name)
+# Update the code below this line to solve the problem
+a=23
+b=20
+print(a+b)
